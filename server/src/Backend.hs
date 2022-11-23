bc757a6cc6a0de1e70095851768be134dfe38cc7
Xinsong Lin <zhongjiezhex12@me.com>
2022-11-23 12:52:17 -0800

    broadcast works WIP

--- server/src/Backend.hs
+++ server/src/Backend.hs
@@ -2,29 +2,71 @@ module Backend (server) where
 
 import Network.Socket
 import Network.Socket.ByteString (sendAll, recv)
+import Data.Map (Map, empty, fromList, insert, toList)
 import qualified Data.ByteString.Char8 as BS  -- avoids putStrLn conflict
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Control.Monad.State
+
+data Client = Client { 
+  target_sock :: Socket, 
+  progress    :: Double
+}
+  deriving (Show)
 
 server :: IO ()
 server = do
-  sock <- socket AF_INET Stream 0   -- create socket
-  setSocketOption sock ReuseAddr 1  -- closes the socket immediately so can be reused
-  bind sock (SockAddrInet 1234 0)   -- 0 here means accept any IP
-  listen sock 2                     -- wait for clients to connect. 2 for queue size
-  loop sock                         -- now we have created the sock object, pass it into the main loop
+  sock <- socket AF_INET Stream 0         -- create socket
+  setSocketOption sock ReuseAddr 1        -- closes the socket immediately so can be reused
+  bind sock (SockAddrInet 1234 0)         -- 0 here means accept any IP
+  listen sock 2                           -- wait for clients to connect. 2 for queue size
+  players <- execStateT (joinGame sock 2) empty  -- now we have created the sock object, pass it into the main loop
+  -- execStateT (gameLoop sock) players
+  print players
 
-loop :: Socket -> IO ()
-loop sock = do
-  conn <- accept sock               -- connection established
-  handle conn                       -- handle here is the main logic
-  loop sock                         -- recurse until ctrl+c
+signal :: Socket -> String -> IO ()
+signal sock msg = sendAll sock (BS.pack msg)
 
-handle :: (Socket, SockAddr) -> IO ()
-handle (sock, addr) = do
-  print addr                        -- print the client addr
-  byteStr <- recv sock 1024
-  print byteStr
-  sendAll sock $ BS.pack "Hi back!"
-  close sock                        -- close for now for testing
+broadcastProgress :: Map SockAddr Client -> IO ()
+broadcastProgress dict  = helper (toList dict)
+  where
+    helper [] = return ()
+    helper ((_, client):xs) = do
+      signal (target_sock client) (show (progress client))
+      helper xs
+
+joinGame :: Socket -> Int -> StateT (Map SockAddr Client) IO ()
+joinGame _ 0 = return ()
+joinGame sock n = do
+  (client_sock, addr) <- lift (accept sock)
+  -- handle (client_sock, addr)
+  lift (print addr)                        -- print the client addr
+  dict <- get
+  put (insert addr (Client {target_sock = client_sock, progress = -1}) dict)  -- add the client to the map
+  lift (signal client_sock "Welcome to TypeRacer!")
+  updated_dict <- get
+  lift (broadcastProgress updated_dict)
+  -- byteStr <- lift (recv sock 1024)
+  -- lift (print byteStr)
+  -- dict <- get
+  -- _ <- put (insert addr (-1) dict)
+  joinGame sock (n-1)
+
+-- loop :: Socket -> StateT (Map SockAddr Client) IO ()
+-- loop sock = do
+--   conn <- lift (accept sock)  -- connection established
+--   handle conn                       -- handle here is the main logic
+--   loop sock                         -- recurse until ctrl+c
+
+-- handle :: (Socket, SockAddr) -> StateT (Map SockAddr Client) IO ()
+-- handle conn@(sock, addr) = do
+--   lift (print addr)                        -- print the client addr
+--   byteStr <- lift (recv sock 1024)
+--   lift (print byteStr)
+--   lift (sendAll sock (BS.pack "Hi back!"))
+--   byteStr2 <- lift (recv sock 1024)
+--   lift (print byteStr2)
+--   dict <- get
+--   put (insert conn (-1) dict)
+--   lift (sendAll sock (BS.pack "Hi back!"))
+--   -- put (adjust (\k -> a -> a)  (Map k a))
+--   -- lift (close sock)                        -- close for now for testing
--- server/test/test_client.py
+++ server/test/test_client.py
@@ -5,10 +5,16 @@ import socket
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 1234         # The port used by the server
 
-for i in range(5):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    welcome_msg = s.recv(1024)
+    print(welcome_msg)
+    welcome_msg = s.recv(1024)
+    print(welcome_msg)
+    welcome_msg = s.recv(1024)
+    print(welcome_msg)
+    for i in range(5):
         msg = input(f"${i} ")
-        s.connect((HOST, PORT))
         s.sendall(msg.encode("ascii"))
         data = s.recv(1024)
         print(f"Received {data!r}")
