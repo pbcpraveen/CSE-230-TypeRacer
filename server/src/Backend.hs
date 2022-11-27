0975b3d630136443e80a55d8aaf5df3d413f224c
Xinsong Lin <zhongjiezhex12@me.com>
2022-11-27 03:32:06 -0800

    server more or less done

--- server/src/Backend.hs
+++ server/src/Backend.hs
@@ -5,17 +5,21 @@ import Prelude hiding (length)
 import Network.Socket
 import Network.Socket.ByteString (sendAll, recv)
 import Data.Map (Map, empty, adjust, insert, toList)
-import Data.ByteString (length)
 import qualified Data.ByteString.Char8 as BS  -- avoids putStrLn conflict
 
 import Control.Monad.State
 import Control.Concurrent (threadDelay)
 
+import System.Timeout (timeout)
+
 corpus :: String
 corpus = "The quick brown fox jumps over the lazy dog"
 
 delay :: Int
-delay = 1000000  -- 0.1 second
+delay = 1000000  -- 1 second
+
+recvTimeout :: Int
+recvTimeout = 100000  -- 0.1 second
 
 data Client = Client { 
   target_sock :: Socket, 
@@ -31,20 +35,19 @@ server = do
   listen sock 2                                  -- wait for clients to connect. 2 for queue size
   players <- execStateT (joinGame sock 2) empty  -- now we have created the sock object, pass it into the main loop
   broadcastMsg players corpus                    -- send the text to type
-  -- setSocketOption sock RecvTimeOut delay         -- set a low timeout for recv so we can loop through players
   results <- execStateT (gameLoop True) players  -- start the game loop
   print results
 
-signal :: Socket -> String -> IO ()
+signal :: Socket -> String -> IO ()  -- function to send a message to a socket
 signal sock msg = sendAll sock (BS.pack msg)
 
 broadcastProgress :: Map SockAddr Client -> IO ()
-broadcastProgress dict  = helper (toList dict)
+broadcastProgress dict = helper (toList dict) ('$':show dict)
   where
-    helper [] = return ()
-    helper ((_, client):xs) = do
-      signal (target_sock client) (show (progress client))
-      helper xs
+    helper []               _       = return ()
+    helper ((_, client):xs) content = do
+      signal (target_sock client) content
+      helper xs content
 
 broadcastMsg :: Map SockAddr Client -> String -> IO ()
 broadcastMsg dict msg = helper (toList dict)
@@ -53,14 +56,6 @@ broadcastMsg dict msg = helper (toList dict)
     helper ((_, client):xs) = do
       signal (target_sock client) msg
       helper xs
-
--- setLowRecvTimeout :: Map SockAddr Client -> IO ()
--- setLowRecvTimeout dict = helper (toList dict)
---   where
---     helper [] = return ()
---     helper ((_, Client sock _):xs) = do
---       setSocketOption sock RecvTimeOut delay
---       helper xs
 
 joinGame :: Socket -> Int -> StateT (Map SockAddr Client) IO ()
 joinGame _ 0 = do
@@ -77,21 +72,28 @@ joinGame sock n = do
 receiveProgress :: [(SockAddr, Client)] -> StateT (Map SockAddr Client) IO ()
 receiveProgress [] = return ()
 receiveProgress ((addr, Client sock _):xs) = do
-  msg <- lift (recv sock 1024)
-  if length msg == 0
-    then receiveProgress xs
-    else let new_progress = read (BS.unpack msg) :: Double in do
-      dict <- get
-      put (adjust (\(Client s _) -> Client s new_progress) addr dict)
-      receiveProgress xs
+  maybeMsg <- lift (timeout recvTimeout (recv sock 1024))
+  case maybeMsg of
+    Nothing  -> receiveProgress xs  -- just ignore the client if they don't respond
+    Just msg -> do
+      let new_progress = read (BS.unpack msg) :: Double
+      dict <- get  -- get old dict
+      put (adjust (\(Client s _) -> Client s new_progress) addr dict)  -- update dict
+      receiveProgress xs  -- move on
+
+gameShouldContinue :: Map SockAddr Client -> Bool
+gameShouldContinue dict = helper (toList dict)
+  where
+    helper [] = True
+    helper ((_, Client _ prog):xs) = prog < 1 && helper xs
 
 gameLoop :: Bool -> StateT (Map SockAddr Client) IO ()
 gameLoop False = return ()
 gameLoop True  = do
-  lift (putStrLn "looping...")
   dict <- get
+  -- lift (print dict)
   receiveProgress (toList dict)
   updated_dict <- get
   lift (broadcastProgress updated_dict)
-  lift (threadDelay delay)
-  gameLoop True
+  lift (threadDelay delay)  -- this delay lightens the cpu load
+  gameLoop (gameShouldContinue updated_dict)
