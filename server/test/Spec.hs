4274b37a8bb621c6e4504c2a44ebeb3f640e7bb9
Xinsong Lin <zhongjiezhex12@me.com>
2022-12-05 03:53:11 -0800

    more tests

--- server/test/Spec.hs
+++ server/test/Spec.hs
@@ -6,6 +6,20 @@ import Test.Hspec
 import Utils
 import Data.List (sort)
 
+testShuffle :: IO ()
+testShuffle = do
+  let xs = [-1000..1000] :: [Int]
+  ys <- shuffle xs
+  sort ys `shouldBe` xs
+
+testCorpusGen :: IO ()
+testCorpusGen = do
+  corpus1 <- generateCorpus 100
+  length (words corpus1) `shouldBe` 100
+  corpus2 <- generateCorpus 100
+  length (words corpus2) `shouldBe` 100
+  corpus1 `shouldNotBe` corpus2
+
 -- create two clients and connect to the server, expecting to receive the corpus
 testServer :: IO ()
 testServer = do
@@ -23,13 +37,8 @@ testServer = do
   length (words (show corpus2)) `shouldBe` 10
   corpus1 `shouldBe` corpus2
 
-testShuffle :: IO ()
-testShuffle = do
-  let xs = [-1000..1000] :: [Int]
-  ys <- shuffle xs
-  sort ys `shouldBe` xs
-
 main :: IO ()
 main = do
   testShuffle
+  testCorpusGen
   testServer
