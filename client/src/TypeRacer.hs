4f1be50e67dbcc8f273f9be245dee034a593b7f5
Xinsong Lin <zhongjiezhex12@me.com>
2022-12-06 01:07:26 -0800

    typing sorta working

--- client/src/TypeRacer.hs
+++ client/src/TypeRacer.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE TemplateHaskell #-}
-module TypeRacer (theMain) where
+module TypeRacer where
 
 import qualified Graphics.Vty as V
 import Lens.Micro.Mtl
@@ -19,61 +19,89 @@ import Brick.Widgets.Core
   ( (<+>), (<=>)
   , str
   , updateAttrMap
-  , overrideAttr
+  , overrideAttr, withAttr
   )
 import Brick.Util (fg, bg, on, clamp)
 import qualified Brick.BChan
 import qualified Graphics.Vty
 import Control.Concurrent (threadDelay)
 import GHC.Conc (forkIO)
+import Data.Char (toLower)
 
-data MyAppState n = MyAppState { _x, _y, _z :: Float }
+data MyAppState n = MyAppState {
+  _x, _y, _z :: Float,
+  _corpus :: String,
+  _typed :: String,
+  _percentage :: Float
+} deriving (Show)
 
 makeLenses ''MyAppState
+
+partition :: String -> String -> (String, String, String)
+partition target actual = (common, wrong, rest)
+  where
+    commonLen = length $ takeWhile (uncurry (==)) $ zip target actual
+    common = take commonLen actual
+    wrong = drop commonLen actual
+    rest = drop commonLen target
 
 drawUI :: MyAppState () -> [Widget ()]
 drawUI p = [ui]
     where
       -- use mapAttrNames
-      xBar = updateAttrMap
-             (A.mapAttrNames [ (xDoneAttr, P.progressCompleteAttr)
-                             , (xToDoAttr, P.progressIncompleteAttr)
-                             ]
-             ) $ bar $ _x p
-      -- or use individual mapAttrName calls
-      yBar = updateAttrMap
-             (A.mapAttrName yDoneAttr P.progressCompleteAttr .
-              A.mapAttrName yToDoAttr P.progressIncompleteAttr) $
-             bar $ _y p
-      -- or use overrideAttr calls
-      zBar = overrideAttr P.progressCompleteAttr zDoneAttr $
-             overrideAttr P.progressIncompleteAttr zToDoAttr $
-             bar $ _z p
-      lbl c = Just $ show $ fromEnum $ c * 100
-      bar v = P.progressBar (lbl v) v
-      ui = (str "X: " <+> xBar) <=>
-           (str "Y: " <+> yBar) <=>
-           (str "Z: " <+> zBar) <=>
-           str "" <=>
-           str "Hit 'x', 'y', or 'z' to advance progress, or 'q' to quit"
+      -- xBar = updateAttrMap
+      --        (A.mapAttrNames [ (xDoneAttr, P.progressCompleteAttr)
+      --                        , (xToDoAttr, P.progressIncompleteAttr)
+      --                        ]
+      --        ) $ bar $ _x p
+      -- -- or use individual mapAttrName calls
+      -- yBar = updateAttrMap
+      --        (A.mapAttrName yDoneAttr P.progressCompleteAttr .
+      --         A.mapAttrName yToDoAttr P.progressIncompleteAttr) $
+      --        bar $ _y p
+      -- -- or use overrideAttr calls
+      -- zBar = overrideAttr P.progressCompleteAttr zDoneAttr $
+      --        overrideAttr P.progressIncompleteAttr zToDoAttr $
+      --        bar $ _z p
+      -- lbl c = Just $ show $ fromEnum $ c * 100
+      -- bar v = P.progressBar (lbl v) v
+      (common, wrong, rest) = partition (_corpus p) (_typed p)
+      common' = withAttr correctAttr $ str common
+      wrong' = withAttr wrongAttr $ str wrong
+      rest' = withAttr restAttr $ str rest
+      ui = --(str "X: " <+> xBar) <=>
+           --(str "Y: " <+> yBar) <=>
+           --(str "Z: " <+> zBar) <=>
+           common' <+> wrong' <+> rest'
+
 
 data TimerEvent = Interrupt deriving (Show)
-
-myEventHandler :: T.BrickEvent () TimerEvent -> T.EventM () (MyAppState ()) ()
-myEventHandler _ = 
-       let valid = clamp (0.0 :: Float) 1.0 in
-       z %= valid . (+ 0.01)
 
 valid :: Float -> Float
 valid = clamp (0.0 :: Float) 1.0
 
+computePercentage :: String -> String -> Float
+computePercentage target actual = fromIntegral common / fromIntegral len
+  where
+    common = length $ takeWhile (uncurry (==)) $ zip target actual
+    len = length target
+
 appEventHandler :: T.BrickEvent () TimerEvent -> T.EventM () (MyAppState ()) ()
 appEventHandler (T.AppEvent Interrupt) = z %= valid . (+ 0.01)
 appEventHandler (T.VtyEvent e) = case e of
-  V.EvKey (V.KChar 'x') [] -> x %= valid . (+ 0.05)
-  V.EvKey (V.KChar 'y') [] -> y %= valid . (+ 0.03)
-  V.EvKey (V.KChar 'z') [] -> z %= valid . (+ 0.02)
-  V.EvKey (V.KChar 'q') [] -> M.halt
+  V.EvKey V.KEsc        [] -> M.halt
+  V.EvKey V.KBS         [] -> do
+    before <- use typed
+    let after = take (length before - 1) before
+    typed .= after
+    target <- use corpus
+    actual <- use typed
+    percentage .= computePercentage target actual
+  V.EvKey (V.KChar  c ) [] -> do
+    typed %= (++ [c])
+    target <- use corpus
+    actual <- use typed
+    percentage .= computePercentage target actual
   _                        -> return ()
 appEventHandler _ = return ()
 
@@ -92,14 +120,26 @@ zDoneAttr, zToDoAttr :: A.AttrName
 zDoneAttr = theBaseAttr <> A.attrName "Z:done"
 zToDoAttr = theBaseAttr <> A.attrName "Z:remaining"
 
+correctAttr :: A.AttrName
+correctAttr = theBaseAttr <> A.attrName "correct"
+
+wrongAttr :: A.AttrName
+wrongAttr = theBaseAttr <> A.attrName "wrong"
+
+restAttr :: A.AttrName
+restAttr = theBaseAttr <> A.attrName "rest"
+
 theMap :: A.AttrMap
 theMap = A.attrMap V.defAttr
-         [ (theBaseAttr,               bg V.brightBlack)
+         [ (theBaseAttr,               bg V.white)
          , (xDoneAttr,                 V.black `on` V.white)
          , (xToDoAttr,                 V.white `on` V.black)
          , (yDoneAttr,                 V.magenta `on` V.yellow)
          , (zDoneAttr,                 V.blue `on` V.green)
          , (zToDoAttr,                 V.blue `on` V.red)
+         , (correctAttr,               fg V.green)
+         , (wrongAttr,                 fg V.red)
+         , (restAttr,                  fg $ V.RGBColor 200 200 200)
          , (P.progressIncompleteAttr,  fg V.yellow)
          ]
 
@@ -113,7 +153,7 @@ theApp =
           }
 
 initialState :: MyAppState ()
-initialState = MyAppState 0.25 0.18 0.63
+initialState = MyAppState 0.25 0.18 0.63 "Hello, world!" "" 0.0
 
 interruptThread :: Brick.BChan.BChan TimerEvent -> IO ()
 interruptThread chan = do
@@ -129,4 +169,5 @@ theMain = do
   let buildVty = Graphics.Vty.mkVty Graphics.Vty.defaultConfig
   initialVty <- buildVty
   finalState <- M.customMain initialVty buildVty (Just eventChan) theApp initialState
+  print finalState
   return ()
--- client/test/Spec.hs
+++ client/test/Spec.hs
@@ -1,2 +1,18 @@
+import Test.Hspec
+
+import TypeRacer
+
+testPartition :: IO ()
+testPartition = do
+  let target = "hello world"
+  let actual = "hello123"
+  let (correct, wrong, rest) = partition target actual
+  putStrLn correct
+  putStrLn wrong
+  putStrLn rest
+  correct `shouldBe` "hello"
+  wrong `shouldBe` "123"
+  rest `shouldBe` " world"
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = testPartition
